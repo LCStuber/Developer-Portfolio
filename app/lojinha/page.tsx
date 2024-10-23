8bbbab3dc67632fae347d8025e77cdab276b1802
Leonardo Cazotto Stuber <leonardo_stu@outlook.com.br>
2024-10-23 08:59:28 -0300

    ADD: "lojinha" path

---
+++ app/lojinha/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { useEffect } from 'react';
+
+export default function TrocaDeTurnoIMT() {
+  useEffect(() => {
+    window.location.href = 'https://docs.google.com/spreadsheets/d/1Hj1MW6kdXAsUSdasmj-oomm8wkrnSlKfeYmvWkqayJ0/edit?usp=sharing';
+  }, []);
+
+  return null;
+}
